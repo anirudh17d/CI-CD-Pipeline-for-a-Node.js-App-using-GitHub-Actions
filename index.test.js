9e2aeb8c916fd8635e7576ed2118bdf00fed49ef
Anirudh Bhargav <anirudhdb@gmail.com>
2025-08-04 11:48:19 +0200

    Add GitHub Actions CI workflow

---
+++ index.test.js
@@ -0,0 +1,9 @@
+// index.test.js
+const request = require("supertest");
+const app = require("./index");
+
+test("GET / should return Hello CI/CD!", async () => {
+  const response = await request(app).get("/");
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe("Hello CI/CD!");
+});
