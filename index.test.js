24eab9ec6911fc8175f17dbc0ffa1c434b8c416b
Anirudh Bhargav <anirudhdb@gmail.com>
2025-08-04 11:53:29 +0200

    failure test

--- index.test.js
+++ index.test.js
@@ -5,5 +5,5 @@ const app = require("./index");
 test("GET / should return Hello CI/CD!", async () => {
   const response = await request(app).get("/");
   expect(response.statusCode).toBe(200);
-  expect(response.text).toBe("Hello CI/CD!");
+  expect(response.text).toBe("This will fail");
 });
